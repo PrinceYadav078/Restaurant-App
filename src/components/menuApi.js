e9ba0df826cb6d424d1801317dba215eeffabbd5
princeyadav283203@gmail.com <princeyadav283203@gmail.com>
2023-08-15 14:01:30 +0530

    img bug resolved

--- src/components/menuApi.js
+++ src/components/menuApi.js
@@ -1,7 +1,7 @@
 const Menu = [
     {
       id: 1,
-      image: "images/maggi.jpg",
+      image: "https://thumbs.dreamstime.com/z/indian-instant-masala-atta-noodles-vegetables-191692006.jpg?w=992",
       name: "maggi",
       category: "breakfast",
       price: "12₹",
@@ -11,7 +11,7 @@ const Menu = [
   
     {
       id: 2,
-      image: "images/allupakoida.jpg",
+      image: "https://t4.ftcdn.net/jpg/01/16/77/23/240_F_116772328_oRqThvo9RhoY27SOzeLpXnfUiDFyhTYM.jpg",
       name: "allu pakoida",
       category: "evening",
       price: "20₹",
@@ -20,7 +20,7 @@ const Menu = [
     },
     {
       id: 3,
-      image: "images/corn.jpg",
+      image: "https://assets.bonappetit.com/photos/6283b5cd59cc9bcfbef829b1/1:1/w_1821,h_1822,c_limit/Grilled-Corn-With-Chaat-Masala.png",
       name: "corn",
       category: "breakfast",
       price: "10₹",
@@ -29,7 +29,7 @@ const Menu = [
     },
     {
       id: 4,
-      image: "../images/chola.jpg",
+      image: "https://ranveerbrar.com/wp-content/uploads/2022/03/Without-soak-chole-masala.jpg",
       name: "chola",
       category: "lunch",
       price: "50₹",
@@ -38,7 +38,7 @@ const Menu = [
     },
     {
       id: 5,
-      image: "../images/pizza.jpg",
+      image: "https://img.freepik.com/free-photo/freshly-italian-pizza-with-mozzarella-cheese-slice-generative-ai_188544-12347.jpg?w=2000",
       name: "pizza",
       category: "evening",
       price: "80₹",
@@ -47,7 +47,7 @@ const Menu = [
     },
     {
       id: 6,
-      image: "../images/nonvegthali.jpg",
+      image: "https://ar.saveri.ae/wp-content/uploads/2022/08/Saveri-Non-Veg-Thali.jpg",
       name: "Non-Veg Thali",
       category: "dinner",
       price: "180₹",
@@ -56,7 +56,7 @@ const Menu = [
     },
     {
       id: 7,
-      image: "../images/sweet.jpg",
+      image: "https://classicradheysweets.in/cdn/shop/products/KHOYAMIXSWEETS.png?v=1629350093",
       name: "Sweets",
       category: "dinner",
       price: "60₹",
@@ -65,7 +65,7 @@ const Menu = [
     },
     {
       id: 8,
-      image: "../images/rajmarice.jpg",
+      image: "https://sandhyahariharan.co.uk/wp-content/uploads/2010/11/Rajma-Masala-1-of-1-500x500.jpg",
       name: "Rajma Rice",
       category: "lunch",
       price: "60₹",
@@ -74,7 +74,7 @@ const Menu = [
     },
     {
       id: 9,
-      image: "../images/samosa.jpg",
+      image: "https://www.shutterstock.com/shutterstock/photos/2067201449/display_1500/stock-photo-veg-samosa-is-a-crispy-and-spicy-indian-triangle-shape-snack-which-has-crisp-outer-layer-of-maida-2067201449.jpg",
       name: "samaso",
       category: "evening",
       price: "10₹",
